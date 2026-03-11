0683926982793eb7fbf3cd323fdc6b1c97fb89ac
Kartavikoya <www.kartavikoya456@gmail.com>
2026-03-11 16:50:10 +0530

    Added UpdatingObjects

--- src/hooks/useState/UpdatingObjects.jsx
+++ src/hooks/useState/UpdatingObjects.jsx
@@ -1,16 +1,54 @@
 import { useState } from "react";
 
 function Obj(){
-  const [serach, setSearch] = useState();
+//   const [serach, setSearch] = useState();
 
-  const OnSearchChange = (event) =>{
-    setSearch(event.target.value)
-  }
+    const initalState = {
+        name:'',
+        email:'',
+        password:'',
+        mobile:'',
+        altNumber:''
+
+
+    }
+    const [form, setForm] = useState(initalState);
+
+//   const OnSearchChange = (event) =>{
+//     setSearch(event.target.value)
+//   }
+    const onNameChange = (e) =>{
+        setForm({ ... form, name: e.target.value})
+    }
+    const onEmailChange = (e) =>{
+        setForm({ ... form, email: e.target.value})
+    }
+    // console.log(form);
+    
 
   return (
     <>
-    <input onChange={OnSearchChange} placeholder="something..." />
-    <p>{serach}</p>
+    <div>
+        <label htmlFor="">Name</label>
+        <input  onChange= {onNameChange}placeholder="Please enter your name" />
+    </div>
+     <div>
+        <label htmlFor="">Email</label>
+        <input  onChange={onEmailChange} placeholder="Please enter your email" />
+    </div>
+     <div>
+        <label htmlFor="">Password</label>
+        <input  placeholder="Please enter your password" />
+    </div>
+     <div>
+        <label htmlFor="">Mobile Number</label>
+        <input  placeholder="Please enter your mobile number" />
+    </div>
+     <div>
+        <label htmlFor="">Alternate Number</label>
+        <input  placeholder="Please enter your alternate number" />
+    </div>
+
     </>
   );
 }
