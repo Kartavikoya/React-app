162a9cd822980d232c80b81f06e1bb5fcbf25e33
Kartavikoya <www.kartavikoya456@gmail.com>
2026-04-10 16:39:52 +0530

    NavLink added

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,17 @@ import UseReducerExamples from './hooks/useReducer/UseReducerExample.jsx';
 import UseContext from './hooks/useContext/Exmple.jsx';
 // import E1 from './hooks/contextandreducer/FirstExample.jsx';
 import Redu1 from './ReduxExample/reduxexmp.jsx';
-import { Link, Route, Routes } from 'react-router-dom'; 
+import { Link, NavLink, Route, Routes } from 'react-router-dom'; 
 import {Home} from './pages/Home/index.jsx';
 import {Cart} from './pages/Cart/index.jsx';
 import {About} from './pages/About/index.jsx';
 
 function App() {
+  const getStyles =({isActive}) => {
+    return{
+      color:isActive ? 'red' : ''
+    }
+  }
   return (
     <>
       {/* show examples and props component */}
@@ -31,9 +36,12 @@ function App() {
       {/* <Redu1/> */}
       <div className='App'>
         {/* <a href="/">Home</a> */}
-        <Link to='/'>Home</Link>||
+        {/* <Link to='/'>Home</Link>||
         <Link to="/cart">Cart</Link>||        
-        <Link to="/about">About</Link>||
+        <Link to="/about">About</Link>|| */}
+        <NavLink to ='' style={getStyles}>Home</NavLink>
+        <NavLink to ='/cart' style={getStyles}>Cart</NavLink>
+        <NavLink to ='/about' style={getStyles}>About</NavLink>
         <Routes>
           <Route path='/' element ={<Home/>}/>
           <Route path='/cart' element ={<Cart/>}/>
